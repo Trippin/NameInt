6057b47b5552c23de3778b9f7e69bef62c1b6449
Trippin <90400991+Trippin@users.noreply.github.com>
2022-09-20 16:43:57 -0400

    Add files via upload

---
+++ nameint.py
@@ -0,0 +1,23 @@
+first_initial = """
+      JJJJJJJJJJ
+        JJJJJ
+        JJJJJ
+        JJJJJ
+        JJJJJ
+JJJJJ   JJJJJ
+JJJJJ   JJJJJ
+     JJJJJ
+     """
+print(first_initial)
+
+second_initial = """
+HHHHH            HHHHH 
+HHHHH            HHHHH
+HHHHH            HHHHH
+HHHHH   HHHHHHH  HHHHH
+HHHHH   HHHHHHH  HHHHH
+HHHHH            HHHHH
+HHHHH            HHHHH
+HHHHH            HHHHH
+"""
+print(second_initial)
